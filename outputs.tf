de4ffa7e9780be7f21b8365e152a9c4cf3583bfa
Sandeepkumarsingh13 <96614521+Sandeepkumarsingh13@users.noreply.github.com>
2021-12-24 08:59:46 +0000

    Add files via upload

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,23 @@
-output "s3_bucket_arn" {
-  value       = aws_s3_bucket.mybucket2.arn
-  description = "arn of the S3 bucket"
+# Output variable definitions
+
+output "vpc_public_subnets" {
+  description = "IDs of the VPC's public subnets"
+  value       = module.vpc.public_subnets
 }
 
-output "dynamodb_table_name" {
-  value       = aws_dynamodb_table.locks.name
-  description = "The name of the DynamoDB table"
-  
+output "ec2_instance_public_ips" {
+  description = "Public IP addresses of EC2 instances"
+  value       = module.ec2_instances.public_ip
 }
+
+output "website_bucket_arn" {
+  description = "ARN of the bucket"
+  value       = module.website_s3_bucket.arn
+}
+
+output "website_bucket_name" {
+  description = "Name of the bucket"
+  value       = module.website_s3_bucket.name
+}
+
+
